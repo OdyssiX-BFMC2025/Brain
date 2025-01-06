e958a98ab990bdcebec6d8f5a31d35baf05d16c9
Jash7447 <jashparikh7447@gmail.com>
2025-01-06 13:23:34 +0530

    path change

--- src/dashboard/processDashboard.py
+++ src/dashboard/processDashboard.py
@@ -147,11 +147,11 @@ class processDashboard(WorkerProcess):
         if self.debugging:
             self.logger.info("Received message: " + data)
         dataDict = json.loads(data)
-        with open('/home/pi/Brain/src/utils/table_state.json', 'w') as json_file: # change me(path) 
+        with open('/home/odyssix/Brain/src/utils/table_state.json', 'w') as json_file: # change me(path) 
             json.dump(dataDict, json_file, indent=4)  
 
     def handleLoadTableState(self, data):
-        file_path = '/home/pi/Brain/src/utils/table_state.json' # change me(path)
+        file_path = '/home/odyssix/Brain/src/utils/table_state.json' # change me(path)
         
         try:
             with open(file_path, 'r') as json_file:
