1040a364c50eaadf7359feb7b0f1a7f611da2af8
Jash7447 <jashparikh7447@gmail.com>
2025-02-08 16:39:19 +0530

    debug for subscription6

--- src/lanedetection/lanedetection.py
+++ src/lanedetection/lanedetection.py
@@ -130,7 +130,7 @@ class LaneDetection:
             print("debug: ", self.messages["serialCamera"])
         while True:
             if "serialCamera" in self.messages:
-                image = self.messages["serialCamera"]["obj"].receive()
+                image = self.messages["serialCamera"]["obj"].recv()
             else:
                 print("Key 'serialCamera' not found in messages. Current keys:", self.messages.keys())
 
