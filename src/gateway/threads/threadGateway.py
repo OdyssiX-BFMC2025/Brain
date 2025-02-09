88704caec3ab560e1d7069191bddf55eb4fc1e80
Jash7447 <jashparikh7447@gmail.com>
2025-02-09 16:47:48 +0530

    added a processlanedetection under the workerclass, debug2

--- src/gateway/threads/threadGateway.py
+++ src/gateway/threads/threadGateway.py
@@ -106,8 +106,6 @@ class threadGateway(ThreadWithStop):
             for element in self.sendingList[Owner][Id]:
                 # We send a dictionary that contain the type of the message and message
                 # add a debug message to check if there is a message sent to the pipe of lane dectection reciever.
-                
-                print(f"sedning data to {message['To']['receiver']} pipe -- data ", {"Type": Type, "value": Value, "id": Id, "Owner": Owner})
                 self.sendingList[Owner][Id][element].send(
                     {"Type": Type, "value": Value, "id": Id, "Owner": Owner}
                 )
